9a240d31f7db905c780b2356dc13b1f2019e686c
Ajith27887 <ajithyogesh.n@gmail.com>
2025-09-19 03:16:51 +0530

    cover page - wip

---
+++ src/components/Cover.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./css/Cover.css";
+
+function Cover() {
+	return (
+		<div className="cover">
+			<div className="content">
+				<button>Assignment</button>
+				<div className="heading">
+					<h1>
+						UI Developer <br /> Assignment
+					</h1>
+				</div>
+				<h2>COMPANY</h2>
+				<p>Juspay Technologies Private Limited</p>
+			</div>
+		</div>
+	);
+}
+
+export default Cover;
